ae08e4b76470354b7ca93890eb9f8dbdebcda256
RoseReyes <roseannlorinreyes@gmail.com>
2025-01-19 00:17:00 -0800

    todo list css

--- scripts/todo-lists.js
+++ scripts/todo-lists.js
@@ -1,15 +1,40 @@
-const todoList = [];
+const todoObject = [];
+const todoListContainer = document.querySelector('.js-todo-container');
+const todoDueDate = document.querySelector('.js-due-date');
+
+const renderDom = () => {
+  let todoListHTML = '';
+
+  for (let t = 0; t < todoObject.length; t++) {
+    const todo = todoObject[t];
+    const html = `
+    <p>
+        ${todo.name} 
+        ${todo.date} 
+        <button class="delete" onclick="removeTodo(${t})">Delete</button></p>
+    `;
+    todoListHTML += html;
+  }
+
+  todoListContainer.innerHTML = todoListHTML;
+};
 
 const addTodo = () => {
-  const inputElement = document.querySelector('.js-todo-input');
-  const dateElement = document.querySelector('.js-date-input');
-  const listElement = document.querySelector('.js-todo-list');
-  const todo = inputElement.value;
+  const todoInputElement = document.querySelector('.js-todo-input');
+  const name = todoInputElement.value;
 
-  todoList.push(todo);
-  inputElement.value = '';
+  todoObject.push({
+    name: name,
+    date: todoDueDate.value,
+  });
 
-  const paragraphElement = document.createElement('p');
-  paragraphElement.appendChild(document.createTextNode(todo));
-  listElement.appendChild(paragraphElement);
+  todoInputElement.value = '';
+  todoDueDate.value = '';
+
+  renderDom();
+};
+
+const removeTodo = (index) => {
+  todoList.splice(index, 1);
+  renderDom();
 };
