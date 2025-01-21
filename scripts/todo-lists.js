4495e4e1000b34debc5e7c108439806e1ce3c205
Rose Reyes <36457350+RoseReyes@users.noreply.github.com>
2025-01-20 19:38:37 -0800

    Revert "init commit"

--- scripts/todo-lists.js
+++ scripts/todo-lists.js
@@ -65,3 +65,24 @@ const saveToStorage = () => {
 document.querySelector('.js-add-todo').addEventListener('click', () => {
   addTodo();
 });
+
+const multiplyNums = (num1, num2) => num1 * num2;
+console.log(multiplyNums(2, 3));
+
+const addNum = (array, num) => array.map((value) => value + num);
+console.log(addNum([-2, -1, 0, 99], 2));
+
+const removeEgg = (array) => {
+  let eggsCount = 0;
+
+  return array.filter((value, index) => {
+    if (value === 'egg' && eggsCount < 2) {
+      eggsCount++;
+      return false;
+    }
+
+    return true;
+  });
+};
+
+console.log(removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']));
