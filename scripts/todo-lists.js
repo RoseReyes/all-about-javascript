fda2acf67d65328d454e17b8c9797d4a2f836bd5
RoseReyes <roseannlorinreyes@gmail.com>
2025-01-20 19:27:40 -0800

    advanced functions practice

--- scripts/todo-lists.js
+++ scripts/todo-lists.js
@@ -10,16 +10,24 @@ const renderToDom = () => {
   let todoListHTML = '';
 
   todoList.forEach((todoObject, index) => {
+    itemToDelete = index;
     const { name, dueDate } = todoObject;
     const html = `
       <div>${name}</div>
       <div>${dueDate}</div>
-      <button class="delete-todo-button" onclick="removeTodo(${index})">Delete</button>
+      <button class="js-delete-todo delete-todo-button">Delete</button>
       `;
     todoListHTML += html;
   });
 
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+  document
+    .querySelectorAll('.js-delete-todo')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        removeTodo(index);
+      });
+    });
 };
 
 renderToDom();
@@ -53,3 +61,28 @@ const removeTodo = (index) => {
 const saveToStorage = () => {
   localStorage.setItem('todoList', JSON.stringify(todoList));
 };
+
+document.querySelector('.js-add-todo').addEventListener('click', () => {
+  addTodo();
+});
+
+const multiplyNums = (num1, num2) => num1 * num2;
+console.log(multiplyNums(2, 3));
+
+const addNum = (array, num) => array.map((value) => value + num);
+console.log(addNum([-2, -1, 0, 99], 2));
+
+const removeEgg = (array) => {
+  let eggsCount = 0;
+
+  return array.filter((value, index) => {
+    if (value === 'egg' && eggsCount < 2) {
+      eggsCount++;
+      return false;
+    }
+
+    return true;
+  });
+};
+
+console.log(removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']));
