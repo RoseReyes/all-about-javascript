2a801f5ff1a7709661045e4fd7b8061771acc687
RoseReyes <roseannlorinreyes@gmail.com>
2025-01-25 22:58:10 -0800

    rename test-jasmine to tests

--- amazon/tests/checkout/orderSummaryTest.js
+++ amazon/tests/checkout/orderSummaryTest.js
@@ -2,13 +2,16 @@ import { renderOrderSummary } from '../../scripts/checkout/orderSummary.js';
 import { loadFromStorage, cart } from '../../data/cart.js';
 
 describe('Test suite: render order summary', () => {
-  it('displays the cart', () => {
+  const id1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+  const id2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+
     document.querySelector('.js-test-container').innerHTML = `
         <div class="js-order-summary"></div>
+        <div class="js-payment-summary"></div>
         `;
-
-    const id1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-    const id2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
     // mock the data from local storage
     spyOn(localStorage, 'getItem').and.callFake(() => {
@@ -27,8 +30,15 @@ describe('Test suite: render order summary', () => {
     });
 
     loadFromStorage();
-    renderOrderSummary();
 
+    renderOrderSummary();
+  });
+
+  afterEach(() => {
+    document.querySelector('.js-test-container').innerHTML = '';
+  });
+
+  it('displays the cart', () => {
     expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(
       2
     );
@@ -43,34 +53,6 @@ describe('Test suite: render order summary', () => {
   });
 
   it('removes a product', () => {
-    document.querySelector('.js-test-container').innerHTML = `
-        <div class="js-order-summary"></div>
-        <div class="js-payment-summary"></div>
-        `;
-
-    const id1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
-    const id2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
-
-    // mock the data from local storage
-    spyOn(localStorage, 'getItem').and.callFake(() => {
-      return JSON.stringify([
-        {
-          id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-          quantity: 2,
-          deliveryOptionId: '1',
-        },
-        {
-          id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-          quantity: 1,
-          deliveryOptionId: '2',
-        },
-      ]);
-    });
-
-    loadFromStorage();
-
-    renderOrderSummary();
-
     document.querySelector(`.js-delete-link-${id1}`).click();
     expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(
       1
@@ -83,6 +65,7 @@ describe('Test suite: render order summary', () => {
     expect(
       document.querySelector(`.js-cart-item-container-${id2}`)
     ).not.toEqual(null);
+
     expect(cart.length).toEqual(1);
     expect(cart[0].id).toEqual(id2);
   });
