c2523d730713d258026f11f761e1fd7ac06acd27
RoseReyes <roseannlorinreyes@gmail.com>
2025-01-26 23:41:56 -0800

    convert load products from xhr to fetch

--- amazon/scripts/checkout.js
+++ amazon/scripts/checkout.js
@@ -2,13 +2,9 @@ import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
-import { loadProducts } from '../data/products.js';
+import { loadProductsFetch } from '../data/products.js';
 
-new Promise((resolve) => {
-  loadProducts(() => {
-    resolve();
-  });
-}).then(() => {
+Promise.all([loadProductsFetch()]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
